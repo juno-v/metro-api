5aaf51d968170ed69d4e2639608bc8ff688da33c
Juno <juno.vue@hotmail.com>
2020-11-17 10:20:39 -0600

    added hear

---
+++ metro-app/src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <img src="https://www.metrotransit.org/img/MetroTransitLogo.svg" alt="Metro Transit Logo" /> 
+        </div>
+    );
+};
+
+
+export default Header;
