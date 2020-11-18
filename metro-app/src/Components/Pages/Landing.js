eb19cc8848afe04b2fb2be457dfffa097ebc9c13
Juno <juno.vue@hotmail.com>
2020-11-17 22:50:03 -0600

    have basic UI for all the routes

--- metro-app/src/Components/Pages/Landing.js
+++ metro-app/src/Components/Pages/Landing.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import LightRail from "../LightRail/LightRail";  
+import Routes from "../Routes/Routes"; 
 
 const Landing = () => {
     return (
         <div>
             <LightRail /> 
+            <Routes /> 
         </div>
     );
 };
